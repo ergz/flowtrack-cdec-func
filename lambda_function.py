61742cc8e53bbe8f35fe153c607d16585439bb14
ergz <erodriguez@hey.com>
2024-10-20 00:02:07 -0700

    removes template code

--- lambda_function.py
+++ lambda_function.py
@@ -77,27 +77,6 @@ def insert_cdec_records(conn, data):
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
     try:
         db_secret = get_secret()
         connection_string = f"postgresql://{db_secret['username']}:{db_secret['password']}@{db_host}:{db_port}/flowtrack"
